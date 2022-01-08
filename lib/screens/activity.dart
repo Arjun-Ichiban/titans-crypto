cfe1e54c98bba818b9f659b27c79ea4b31cc7ccb
Arjun S <83405143+Arjun-Ichiban@users.noreply.github.com>
2022-01-08 21:44:26 +0530

    Display the coin transaction data received from server

--- lib/screens/activity.dart
+++ lib/screens/activity.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:titans_crypto/models/coin_transaction_model.dart';
+import 'package:titans_crypto/services/transaction_service.dart';
 
 class ActivityScreen extends StatefulWidget {
   const ActivityScreen({Key? key}) : super(key: key);
@@ -8,6 +10,14 @@ class ActivityScreen extends StatefulWidget {
 }
 
 class _ActivityScreenState extends State<ActivityScreen> {
+  Future<List<CoinTransaction>>? coinTransactionData;
+
+  @override
+  void initState() {
+    super.initState();
+    coinTransactionData = coinTransactionList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -136,7 +146,8 @@ class _ActivityScreenState extends State<ActivityScreen> {
                           Spacer(),
                           IconButton(
                             onPressed: () {
-                              Navigator.of(context).pushNamed('walletTransactionScreen');
+                              Navigator.of(context)
+                                  .pushNamed('walletTransactionScreen');
                             },
                             icon: const Icon(
                               Icons.arrow_forward,
@@ -161,111 +172,134 @@ class _ActivityScreenState extends State<ActivityScreen> {
                 ),
                 SizedBox(
                   height: 400,
-                  child: ListView.builder(
-                    itemBuilder: (context, position) {
-                      return Container(
-                        padding: const EdgeInsets.only(top: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.network(
-                              "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
-                              height: 42,
-                              width: 42,
-                            ),
-                            const SizedBox(
-                              width: 13,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const [
-                                Text(
-                                  "BTC/INR",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                    fontSize: 14,
+                  child: FutureBuilder<List<CoinTransaction>>(
+                    future: coinTransactionData,
+                    builder: (context, snapshot) {
+
+                      if (snapshot.hasData) {
+                        List<CoinTransaction>? data = snapshot.data;
+
+                        return ListView.builder(
+                          itemCount: data?.length,
+                          itemBuilder: (context, index) {
+                            Color transactionColor;
+
+                            if ((data?[index].type ?? '') == 'buy') {
+                              transactionColor = const Color(0xff5ED5A8);
+                            } else {
+                              transactionColor = const Color(0xffDD4B4B);
+                            }
+                            return Container(
+                              padding: const EdgeInsets.only(top: 20),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.network(
+                                    data?[index].imageUrl ?? '',
+                                    height: 42,
+                                    width: 42,
                                   ),
-                                ),
-                                SizedBox(
-                                  height: 5,
-                                ),
-                                Text(
-                                  "Amount",
-                                  style: TextStyle(
-                                    color: Color(0xff777777),
-                                    fontSize: 14,
+                                  const SizedBox(
+                                    width: 13,
                                   ),
-                                ),
-                                SizedBox(
-                                  height: 5,
-                                ),
-                                Text(
-                                  "Price",
-                                  style: TextStyle(
-                                    color: Color(0xff777777),
-                                    fontSize: 14,
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "${data?[index].symbol?.toUpperCase() ?? ''}/INR",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        "Amount",
+                                        style: TextStyle(
+                                          color: Color(0xff777777),
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        "Price",
+                                        style: TextStyle(
+                                          color: Color(0xff777777),
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        "Status",
+                                        style: TextStyle(
+                                          color: Color(0xff777777),
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ),
-                                SizedBox(
-                                  height: 5,
-                                ),
-                                Text(
-                                  "Status",
-                                  style: TextStyle(
-                                    color: Color(0xff777777),
-                                    fontSize: 14,
+                                  const Spacer(),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children:  [
+                                      Text(
+                                        data?[index].date ?? '',
+                                        style: TextStyle(
+                                          color: Color(0xff777777),
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        data?[index].noOfCoins.toString() ?? '',
+                                        style: TextStyle(
+                                          color: transactionColor,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        data?[index].amount.toString() ?? '',
+                                        style: TextStyle(
+                                          color: Color(0xff777777),
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        data?[index].type?.toUpperCase() ?? '',
+                                        style: TextStyle(
+                                          color: transactionColor,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ),
-                              ],
-                            ),
-                            const Spacer(),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: const [
-                                Text(
-                                  "2021-08-02 04:39:26",
-                                  style: TextStyle(
-                                    color: Color(0xff777777),
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 5,
-                                ),
-                                Text(
-                                  "0.49975",
-                                  style: TextStyle(
-                                    color: Color(0xff5ED5A8),
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 5,
-                                ),
-                                Text(
-                                  "2652.00",
-                                  style: TextStyle(
-                                    color: Color(0xff777777),
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 5,
-                                ),
-                                Text(
-                                  "Buy",
-                                  style: TextStyle(
-                                    color: Color(0xff5ED5A8),
-                                    fontSize: 14,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      );
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
+                      } else {
+                        return const Center(child: CircularProgressIndicator());
+                      }
                     },
                   ),
                 ),
@@ -277,3 +311,111 @@ class _ActivityScreenState extends State<ActivityScreen> {
     );
   }
 }
+
+// ListView.builder(
+// itemBuilder: (context, position) {
+// return Container(
+// padding: const EdgeInsets.only(top: 20),
+// child: Row(
+// mainAxisAlignment: MainAxisAlignment.start,
+// crossAxisAlignment: CrossAxisAlignment.start,
+// children: [
+// Image.network(
+// "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+// height: 42,
+// width: 42,
+// ),
+// const SizedBox(
+// width: 13,
+// ),
+// Column(
+// crossAxisAlignment: CrossAxisAlignment.start,
+// children: const [
+// Text(
+// "BTC/INR",
+// style: TextStyle(
+// fontWeight: FontWeight.bold,
+// color: Colors.white,
+// fontSize: 14,
+// ),
+// ),
+// SizedBox(
+// height: 5,
+// ),
+// Text(
+// "Amount",
+// style: TextStyle(
+// color: Color(0xff777777),
+// fontSize: 14,
+// ),
+// ),
+// SizedBox(
+// height: 5,
+// ),
+// Text(
+// "Price",
+// style: TextStyle(
+// color: Color(0xff777777),
+// fontSize: 14,
+// ),
+// ),
+// SizedBox(
+// height: 5,
+// ),
+// Text(
+// "Status",
+// style: TextStyle(
+// color: Color(0xff777777),
+// fontSize: 14,
+// ),
+// ),
+// ],
+// ),
+// const Spacer(),
+// Column(
+// crossAxisAlignment: CrossAxisAlignment.end,
+// children: const [
+// Text(
+// "2021-08-02 04:39:26",
+// style: TextStyle(
+// color: Color(0xff777777),
+// fontSize: 14,
+// ),
+// ),
+// SizedBox(
+// height: 5,
+// ),
+// Text(
+// "0.49975",
+// style: TextStyle(
+// color: Color(0xff5ED5A8),
+// fontSize: 14,
+// ),
+// ),
+// SizedBox(
+// height: 5,
+// ),
+// Text(
+// "2652.00",
+// style: TextStyle(
+// color: Color(0xff777777),
+// fontSize: 14,
+// ),
+// ),
+// SizedBox(
+// height: 5,
+// ),
+// Text(
+// "Buy",
+// style: TextStyle(
+// color: Color(0xff5ED5A8),
+// fontSize: 14,
+// ),
+// ),
+// ],
+// ),
+// ],
+// ),
+// );
+// },
+// ),
